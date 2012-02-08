f91522bab550c95edf8d6010452ca5a2cdf149fd
panagiotis2011 <sch@sch.gr>
2012-02-08 02:10:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LdapApp::Application.config.secret_token = '661ab58568616a691cf1ada3162b40fa627654e4e7820b3caf4095231bd623752e2b2a7c9f49175dfd20351dcd84cea303809a1a427314dd45761d149b4bc028'
